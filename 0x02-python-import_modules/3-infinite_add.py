9b13a20e58493f51114840a0ef8e3dd05dfc89d6
egidooo <kwizeraegide250@gmail.com>
2023-05-11 06:38:11 -0700

    modules and import

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for k in range(len(sys.argv) - 1):
+        result += int(sys.argv[k + 1])
+    print("{}".format(result))
