3081563843ecf4f185800ba79972e94c8b585cad
egidooo <kwizeraegide250@gmail.com>
2023-05-16 10:40:10 -0700

    data structure

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return (None)
+    big_int = my_list[0]
+    for k in range(len(my_list)):
+        if my_list[k] > big_int:
+            big = my_list[k]
+    return (big_int)
