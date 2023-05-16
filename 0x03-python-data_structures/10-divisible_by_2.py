12c05a3053a6e6d06f1ec137a9fc06b52a6505e6
egidooo <kwizeraegide250@gmail.com>
2023-05-16 08:38:53 -0700

    data structure

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 10-divisible_by_2.py
+
+def divisible_by_2(my_list=[]):
+    multiples = []
+    for k in range(len(my_list)):
+        if my_list[k] % 2 == 0:
+            multiples.append(true)
+        else:
+            multiples.append(false)
+    return (multiples)
