d7c0970702db14d278ddd41c159b9bef0e023974
egidooo <kwizeraegide250@gmail.com>
2023-06-05 10:53:08 -0700

    Python - Classes and Objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Square Definition"""
+
+class Square:
+    """Square Represantation"""
+
+    def __init__(self, size):
+        """new Square Initialisation
+        Args:
+        size (int): The size of new Square
+        """
+        self.__size = size
